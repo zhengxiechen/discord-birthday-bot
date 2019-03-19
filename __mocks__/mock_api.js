be10328bb3fc84eab3d1db794a05dd4b7dd83026
Keith Fung <keith.robert.fung@gmail.com>
2019-03-19 02:10:45 -0400

    🔥Delete birthdays.json

--- __mocks__/mock_api.js
+++ __mocks__/mock_api.js
@@ -1,24 +1,27 @@
-const fs = require('fs');
-const birthdayFile = require('./birthdays.json');
-
-
 class MockApi {
+    constructor(){
+        this.birthdays = [
+            {
+                "id": "1",
+                "discord_id": "235088799074484224",
+                "user": "John Smith",
+                "month": "3",
+                "date": "19",
+                "celebrated": "0"
+            }
+        ];
+    }
+
     getBirthdays() {
-        fs.readFile(birthdayFile, (err, data) => {  
-            if (err) throw err;
-            return JSON.parse(data);
-        });
+        return this.birthdays;
     }
 
     updateBirthdayCelebration(discordId, hasCelebrated){
-        // TODO
-        // fs.writeFileSync(birthdayFile, JSON.stringify(birthdays, null, 2), function (err) {
-        //     if (err) return console.log(err);
-        // });
+        this.birthdays[0].celebrated = hasCelebrated ? "1" : "0";
     }
 
     getBirthday(discordId) {
-        // TODO
+        return this.birthdays[0];
     }
 }
 
